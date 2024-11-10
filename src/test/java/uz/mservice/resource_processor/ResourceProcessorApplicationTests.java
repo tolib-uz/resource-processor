b514506c2c1032e8e4ed7d0391155cf0fb5f5db1
Rustam.Yusupov <rustam.r.yu@gmail.com>
2024-11-10 19:48:08 +0500

    init commmit

---
+++ src/test/java/uz/mservice/resource_processor/ResourceProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mservice.resource_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
